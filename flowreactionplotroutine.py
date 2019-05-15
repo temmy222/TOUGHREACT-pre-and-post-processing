69112d6045a0feb3d4cf3055f27684efc2a46bf6
temmy222 <na_me_men@yahoo.com>
2019-05-15 11:51:10 -0500

    routine updates

--- flowreactionplotroutine.py
+++ flowreactionplotroutine.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 14 16:49:02 2019
+Created on Wed May 15 09:02:10 2019
 
 @author: tajayi3
 """
 import matplotlib
 import matplotlib.pyplot as plt
-from t2listing import * 
+from t2listing import *
+import numpy as np
+from scipy import interpolate
+from scipy.interpolate import griddata
 
-class batchreactionplotroutine(object):
+class flowreactionplotroutine(object):
     """
     This class helps in making plots for batch reactions carried out with TOUGHREACT
     
@@ -19,7 +22,7 @@ class batchreactionplotroutine(object):
         An instance of this class takes in three parameters;
         
         filename --> the output file to be interpreted (in tecplot format) e.g. kddconc.tec
-        gridblock ---> the total number of gridblocks in the simulations 
+        gridblock ---> the total number of gridblocks in the simulations trypically should be a single block
         parameters -> parameters to be investigated (should be in a python list) e.g. ['pH','t_na+','t_ca2+']
         """
         self.filename = filename
@@ -31,13 +34,13 @@ class batchreactionplotroutine(object):
         """
         This method converts the list supplied to a dictionary for plot manipulations
         """
-        i = 0
+        i = 1
         param = {}
         for item in self.parameters:
             param[item] = i
             i = i+1
         return param
-
+    
     def threeinone(self,width,height,grid,color='r--'):
         """
         This method plots three figures in a plot window and saves them with the name of the last item on the
@@ -48,23 +51,29 @@ class batchreactionplotroutine(object):
         grid -> the name of the block in the TOUGHREACT flow input
         color -> color is optional but if provided color of plot can be manipulated
         """
-        fig, axs = plt.subplots(1,3, figsize=(width, height), facecolor='w', edgecolor='k')
-        axs = axs.ravel()
+        fig = plt.figure(figsize=(width,height))
         tre = toughreact_tecplot(self.filename,self.gridblock)
         tre.last()
         a = self.parameters
         params = self.convertlisttodict(a)
         for item, number in params.items():
-            mf = tre.history([(grid,item)])
-            time = mf[0]
-            data= mf[1]
-            axs[number].plot(time,data,color, marker='x')
-            axs[number].grid()
-            axs[number].set_ylabel(item)
-            axs[number].set_xlabel('Time (seconds)')
+            ax = fig.add_subplot(1,3,number)
+            data = tre.element[item]
+            X = tre.element['X(m)']
+            Y = tre.element['Y(m)']
+            Z = tre.element['Z(m)']
+            xi,yi = np.meshgrid(X,Y)
+            data1 = griddata((X,Y),data,(xi,yi),method='nearest')
+            #levels = [-4, -3, -2, -1, 1, 2, 3, 4]
+            cs2 = plt.contourf(xi,yi,data1,10,  cmap='jet')  
+            plt.colorbar()
+            plt.xlabel('Distance(m)',fontsize=16)
+            plt.ylabel(item,fontsize=16)
             plt.tight_layout()
+            
+
         fig.savefig(item +'.png',bbox_inches='tight') 
-        
+    
     def sixinone(self,width,height,grid,color='r--'):
         """
         This method plots three figures in a plot window and saves them with the name of the last item on the
@@ -75,19 +84,28 @@ class batchreactionplotroutine(object):
         grid -> the name of the block in the TOUGHREACT flow input
         color -> color is optional but if provided color of plot can be manipulated
         """
-        fig, axs = plt.subplots(2,3, figsize=(width, height), facecolor='w', edgecolor='k')
-        axs = axs.ravel()
+        fig = plt.figure(figsize=(width,height))
         tre = toughreact_tecplot(self.filename,self.gridblock)
         tre.last()
         a = self.parameters
         params = self.convertlisttodict(a)
+        index = [1,2,3,1,2,3]
         for item, number in params.items():
-            mf = tre.history([(grid,item)])
-            time = mf[0]
-            data= mf[1]
-            axs[number].plot(time,data,color, marker='x')
-            axs[number].grid()
-            axs[number].set_ylabel(item)
-            axs[number].set_xlabel('Time (seconds)')
+            if number == 1 or 2 or 3: j = 1
+            elif number == 4 or 5 or 6: j =2
+            ax = fig.add_subplot(j,3,index[number-1])
+            data = tre.element[item]
+            X = tre.element['X(m)']
+            Y = tre.element['Y(m)']
+            Z = tre.element['Z(m)']
+            xi,yi = np.meshgrid(X,Y)
+            data1 = griddata((X,Y),data,(xi,yi),method='nearest')
+            #levels = [-4, -3, -2, -1, 1, 2, 3, 4]
+            cs2 = plt.contourf(xi,yi,data1,10,  cmap='jet')  
+            plt.colorbar()
+            plt.xlabel('Distance(m)',fontsize=16)
+            plt.ylabel(item,fontsize=16)
             plt.tight_layout()
-        fig.savefig(item +'.png',bbox_inches='tight')
+            
+
+        fig.savefig(item +'.png',bbox_inches='tight') 
